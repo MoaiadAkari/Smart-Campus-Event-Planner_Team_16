a5ecbb263aa38589aec057f5c30ba4ade290a944
Moaiad <moayadakkari@gmail.com>
2026-08-09 15:34:34 -0400

    Added schema.sql as well as adding the DB diagram

---
+++ database/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role TEXT NOT NULL
+);
+
+CREATE TABLE events (
+    event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    category TEXT NOT NULL,
+    event_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    location TEXT NOT NULL,
+    capacity INTEGER NOT NULL,
+    event_status TEXT NOT NULL,
+    organizer_id INTEGER NOT NULL,
+    FOREIGN KEY (organizer_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE registrations (
+    registration_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    event_id INTEGER NOT NULL,
+    registration_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    registration_status TEXT NOT NULL,
+
+    UNIQUE (user_id, event_id),
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE
+);
